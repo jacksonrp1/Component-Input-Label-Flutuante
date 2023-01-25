7e8713ed17edc9923d4cd6bffd85596a3a7e135c
Jackson Rodrigues <jacksonrodriguesdepaula@gmail.com>
2023-01-24 21:53:35 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './input.css'
+import React, { useState } from 'react'
+import { FiEye, FiEyeOff } from 'react-icons/fi'
+
+export default function Input({
+  tipoInput = 'text',
+  label = 'Label',
+  corCampoText = '#cfcfcf',
+  corLabel = '#cfcfcf',
+  corEye = '#cfcfcf'
+}) {
+  const [tpInput, setTpInput] = useState(tipoInput)
+  const [visivel, setVisivel] = useState(false)
+
+  const handleEye = () => {
+    setVisivel(!visivel)
+    setTpInput(!visivel ? 'text' : 'password')
+  }
+
+  return (
+    <div className="Input">
+      <input type={tpInput} placeholder=" " style={{ color: corCampoText }} />
+      <p style={{ color: corLabel }}>{label}</p>
+      {tipoInput === 'password' ? (
+        <div onClick={handleEye} className="Input-Type">
+          {visivel ? (
+            <FiEye color={corEye} size={22} />
+          ) : (
+            <FiEyeOff color={corEye} size={22} />
+          )}
+        </div>
+      ) : (
+        ''
+      )}
+      <div className="borderBottom"></div>
+    </div>
+  )
+}
